426b1a0320a03365d251d789f98b3dd8d74d7046
Vladimir Zotov <azuresplash@gmail.com>
2019-02-17 17:03:09 +1100

    Connected to the chat server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChatClient } from "meetup-chat-client";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'workshop-chat-client-angular';
+export class AppComponent implements OnDestroy {
+
+  private client = ChatClient.connect("http://serene-basin-84996.herokuapp.com/");
+
+
+  ngOnDestroy() {
+    this.client.disconnect();
+  }
 }
